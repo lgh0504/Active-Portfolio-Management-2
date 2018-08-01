13c21b39de06e16aa3afe7a992d31d885459f8b6
Theo Turner <theo@Theos-MacBook-Pro.local>
2018-08-01 18:31:37 +0100

    Risk cleanup

--- risk.py
+++ risk.py
@@ -2,7 +2,7 @@ from numpy import std
 
 def risk(ret):
     '''
-    Risk = standard deviation of return.
+    Risk = standard deviation of return. Return is an array.
     The cost of risk is proportional to variance.
     '''
     return std(ret)
