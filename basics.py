a24121227b4c8e9f3ed91afb11c3ba6224060ece
Theo Turner <theo@Theos-MacBook-Pro.local>
2018-08-01 18:57:20 +0100

    Alpha, beta, ir and theoretical alpha

--- basics.py
+++ basics.py
@@ -1,17 +1,42 @@
 from math import sqrt
+from numpy import cov, var
+
+def alpha(ret_p, ret_b):
+    '''
+    Alpha is a measure of returns relative to a benchmark (market), i.e. active
+    return.
+
+    ALPHA = Rp - Rb
+    '''
+    return ret_p - ret_b
+
+def beta(ret_p, ret_b):
+    '''
+    Beta is a measure of volatility relative to a benchmark (market).
+
+    BETA = Cov(Rp, Rb) / Var(Rb)
+    '''
+    covariance = cov(ret_p, ret_b)
+    benchmark_variance = var(ret_b)
+    return covariance / benchmark_variance
 
 def information_ratio(ic, br):
     '''
-    Fundamental law of active management: IR = IC * sqrt(BR)
-    Information Ratio (IR): ratio of expected residual return to its volatility.
+    Information ratio is the ratio of expected residual return to its
+    volatility.
+
+    IR = IC * sqrt(BR)
+    (fundamental law of active management)
     Information Coefficient (IC): our skill in forecasting residual return,
     i.e. correlation between forecasts and the eventual returns.
     Breadth (BR): number of times per year we can use our skill.
     '''
     return ic * sqrt(br)
 
-def alpha(volatility, ic, score):
+def alpha_theoretical(volatility, ic, score):
     '''
+    Alpha is a measure of returns relative to a benchmark (market).
+
     ALPHA = VOLATILITY * IC * SCORE
     Volatility: residual volatility.
     IC: correlation between scores and returns.
--- return.py
+++
@@ -1,5 +0,0 @@
-def active_return(ret_p, ret_b):
-    '''
-    Active return is return relative to a return benchmark.
-    '''
-    return ret_p - ret_b
